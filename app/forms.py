0b34c9569247b35a727b16763e854495c234a155
Eric Kepes <eric@kepes.net>
2016-02-09 18:23:07 -0500

    All of the fields are there now.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,23 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 class SpeakerForm(Form):
-  name = StringField('name', validators=[DataRequired()])
+  availableTracks = [
+    ('Front', 'Front End'),
+    ('Back', 'Back End'),
+    ('DevOps', 'DevOps'),
+    ('Data', 'Data Science'),
+    ('Agile', 'Agile'),
+    ('Business', 'Business of Software')
+  ]
+
   title = StringField('title', validators=[DataRequired()])
   abstract = TextAreaField('abstract', validators=[DataRequired()])
+  time = RadioField('time', choices=[('60','60 Minutes'),('30','30 Minutes')])
+  tracks = StringField('tracks')
+  name = StringField('name', validators=[DataRequired()])
+  email = StringField('email', validators=[DataRequired()])
+  twitter = StringField('twitter')
+  bio = TextAreaField('bio', validators=[DataRequired()])
+  comments = TextAreaField('comments')

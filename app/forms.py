d31356a0de16834fee25bf72b3c2cb9c7ca7b03c
Eric Kepes <eric@kepes.net>
2016-02-10 06:13:18 -0500

    Figured out the magic incantation to import all that I need. Finally.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField, RadioField
+from wtforms import widgets, StringField, TextAreaField, RadioField, SelectMultipleField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class SpeakerForm(Form):
   availableTracks = [
@@ -16,7 +20,7 @@ class SpeakerForm(Form):
   title = StringField('title', validators=[DataRequired()])
   abstract = TextAreaField('abstract', validators=[DataRequired()])
   time = RadioField('time', choices=[('60','60 Minutes'),('30','30 Minutes')])
-  tracks = StringField('tracks')
+  tracks = MultiCheckboxField('tracks', choices=availableTracks)
   name = StringField('name', validators=[DataRequired()])
   email = EmailField('email', validators=[DataRequired()])
   twitter = StringField('twitter')

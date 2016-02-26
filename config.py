254e046a7999d5e25c07d34d9f7a62b1af4ecbb3
Eric Kepes <eric@kepes.net>
2016-02-25 21:49:34 -0500

    dumbass

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ if os.environ.get('XSS_TOKEN') is None:
 else:
   SECRET_KEY = os.environ['XSS_TOKEN']
 
-if os.environ.get('CALL_FOR_SPEAKERS_OPEN') is not None and str.lower(str(os.environ['CALL_FOR_SPEAKERS_OPEN'])):
+if os.environ.get('CALL_FOR_SPEAKERS_OPEN') is not None and str.lower(str(os.environ['CALL_FOR_SPEAKERS_OPEN'])) == 'true':
   CALL_FOR_SPEAKERS_OPEN = True
 else:
   CALL_FOR_SPEAKERS_OPEN = False

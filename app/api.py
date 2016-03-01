60430ae4848132b9ecdf7b7c9966e371a8bf6716
Eric Kepes <eric@kepes.net>
2016-02-29 20:39:22 -0500

    Require a login to access the API

--- app/api.py
+++ app/api.py
@@ -1,10 +1,12 @@
 import datetime
 from flask import redirect, jsonify
+from flask.ext.login import login_required
 from app import app, db
 from .models import Submission
 from config import appConfiguration
 
 @app.route('/submissions', methods=['GET'])
+@login_required
 def get_submissions():
   queryResult = Submission.query.all()
   return jsonify(json_list=[i.serialize for i in queryResult])

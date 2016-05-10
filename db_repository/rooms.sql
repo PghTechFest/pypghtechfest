de5148fafec4dbf71f968ab344b7330f7c321ef3
Eric Kepes <eric@kepes.net>
2016-05-09 21:34:04 -0400

    Scripts for the rooms.

---
+++ db_repository/rooms.sql
@@ -0,0 +1,23 @@
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (1, 1, 'College Center Square');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (2, 2, 'Cafeteria');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (3, 3, 'Ryan Room');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (4, 4, 'Room 317');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (5, 5, 'Room 308');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (6, 6, 'Room 310');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (7, 7, 'Room 311');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (8, 8, 'Room 313');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (9, 9, 'Room 314');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (10, 10, 'Room 315');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (11, 11, 'Room 316');
+
+INSERT INTO room(id, "sortOrder", "roomName") VALUES (12, 12, 'Room 103');
---
+++ db_repository/times.sql
@@ -0,0 +1,21 @@
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (1, 1, '8:30 - 8:45');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (2, 2, '8:45 - 9:15');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (3, 3, '9:30 - 10:30');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (4, 4, '10:45 - 11:15');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (5, 5, '11:30 - 12:30');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (6, 6, '12:30 - 1:30');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (7, 7, '1:30 - 2:30');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (8, 8, '2:45 - 3:45');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (9, 9, '4:00 - 5:00');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (10, 10, '5:00 - 5:15');
+
+INSERT INTO time_slot(id, "sortOrder", "timeSlotName") VALUES (11, 11, '5:15 - 7:00');

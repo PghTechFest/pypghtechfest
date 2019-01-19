1cc3d21c28ac03fce8e85961612f9e5a26855efc
dustinupdyke <dustin@updyke.com>
2019-01-19 17:57:35 -0500

    New site 2019

--- app/js/app.js
+++
@@ -1,9 +0,0 @@
-import SubmissionBox from './SubmissionBox';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <SubmissionBox submissionsUrl="/api/submissions" votesUrl="/api/votes" pollInterval={200000}/>,
-  document.getElementById('reactEntry')
-);
-
